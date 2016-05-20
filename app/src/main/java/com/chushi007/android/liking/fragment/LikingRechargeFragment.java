df6db5c7805b903cfb33147ba27615f38c5a5fc0
huanran <huangran@chushi007.com>
2016-05-20 18:32:40 +0800

    简述：添加首页FragmentTabHost框架
    背景信息：
    
    修改内容：

---
+++ app/src/main/java/com/chushi007/android/liking/fragment/LikingRechargeFragment.java
@@ -0,0 +1,26 @@
+package com.chushi007.android.liking.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.aaron.android.framework.base.BaseFragment;
+
+/**
+ * Created on 16/5/20.
+ *
+ * @author aaron.huang
+ * @version 1.0.0
+ */
+public class LikingRechargeFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText("LikingRechargeFragment");
+        return textView;
+    }
+}
