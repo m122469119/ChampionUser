a9dddc4703f02dfab4ba75fd2cf4f09657258146
shaozucheng <shaozucheng@chushi007.com>
2016-05-23 17:29:21 +0800

    简述：我的界面的一部分
    背景信息：
    
    修改内容：

--- app/src/main/java/com/chushi007/android/liking/fragment/LikingMyFragment.java
+++ app/src/main/java/com/chushi007/android/liking/fragment/LikingMyFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.aaron.android.framework.base.BaseFragment;
+import com.chushi007.android.liking.R;
 
 /**
  * Created on 16/5/20.
@@ -19,8 +20,7 @@ public class LikingMyFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("LikingMyFragment");
-        return textView;
+        View view = inflater.inflate(R.layout.fragment_liking_my,container,false);
+        return view;
     }
 }
