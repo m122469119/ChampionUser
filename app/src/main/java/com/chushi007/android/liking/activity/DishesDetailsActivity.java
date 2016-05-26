7afa0ed17d2e5cce369d5d3758bedcedc2934bda
shaozucheng <shaozucheng@chushi007.com>
2016-05-26 18:19:24 +0800

    简述：周边界面修改，添加关于，成为私教界面
    背景信息：
    
    修改内容：

---
+++ app/src/main/java/com/chushi007/android/liking/activity/DishesDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.chushi007.android.liking.activity;
+
+import android.os.Bundle;
+
+import com.aaron.android.framework.base.actionbar.AppBarActivity;
+import com.chushi007.android.liking.R;
+
+/**
+ * 说明:
+ * Author shaozucheng
+ * Time:16/5/26 下午3:00
+ */
+public class DishesDetailsActivity extends AppBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dishes_details);
+    }
+}
