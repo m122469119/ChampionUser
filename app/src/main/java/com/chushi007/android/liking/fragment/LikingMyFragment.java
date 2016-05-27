6be2a353e9178be2e1c55ae29fa5f78a5fe46421
shaozucheng <shaozucheng@chushi007.com>
2016-05-27 16:25:10 +0800

    简述：添加我的信息界面
    背景信息：
    
    修改内容：

--- app/src/main/java/com/chushi007/android/liking/fragment/LikingMyFragment.java
+++ app/src/main/java/com/chushi007/android/liking/fragment/LikingMyFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.aaron.android.framework.base.BaseFragment;
@@ -15,6 +16,7 @@ import com.chushi007.android.liking.R;
 import com.chushi007.android.liking.activity.AboutActivity;
 import com.chushi007.android.liking.activity.BecomeTeacherActivity;
 import com.chushi007.android.liking.activity.ContactJonInActivity;
+import com.chushi007.android.liking.activity.MyInfoActivity;
 
 /**
  * Created on 16/5/20.
@@ -22,13 +24,14 @@ import com.chushi007.android.liking.activity.ContactJonInActivity;
  * @author aaron.huang
  * @version 1.0.0
  */
-public class LikingMyFragment extends BaseFragment implements View.OnClickListener{
+public class LikingMyFragment extends BaseFragment implements View.OnClickListener {
     private LinearLayout mInviteFriendsLayout;//邀请好友
     private LinearLayout mCouponsLayout;//我的优惠券
     private LinearLayout mContactJoinLayout;//联系加盟
     private LinearLayout mBecomeTeacherLayout;//称为教练
     private LinearLayout mAboutUsLayout;//关于我们
 
+    private RelativeLayout mHeadInfoLayout;
 
     @Nullable
     @Override
@@ -40,6 +43,7 @@ public class LikingMyFragment extends BaseFragment implements View.OnClickListen
     }
 
     private void initView(View view) {
+        mHeadInfoLayout = (RelativeLayout) view.findViewById(R.id.layout_head_info);
         mInviteFriendsLayout = (LinearLayout) view.findViewById(R.id.layout_invite_friends);
         mCouponsLayout = (LinearLayout) view.findViewById(R.id.layout_coupons);
         mContactJoinLayout = (LinearLayout) view.findViewById(R.id.layout_contact_join);
@@ -51,6 +55,7 @@ public class LikingMyFragment extends BaseFragment implements View.OnClickListen
         mContactJoinLayout.setOnClickListener(this);
         mBecomeTeacherLayout.setOnClickListener(this);
         mAboutUsLayout.setOnClickListener(this);
+        mHeadInfoLayout.setOnClickListener(this);
     }
 
 
@@ -66,7 +71,7 @@ public class LikingMyFragment extends BaseFragment implements View.OnClickListen
         ImageView icon = (ImageView) view.findViewById(R.id.standard_my_icon);
         TextView textView = (TextView) view.findViewById(R.id.standard_my_text);
         View line = view.findViewById(R.id.standard_view_line);
-       // icon.setImageDrawable(getResources().getDrawable(drawableResId));
+        // icon.setImageDrawable(getResources().getDrawable(drawableResId));
         textView.setText(text);
         if (isShowLine) {
             line.setVisibility(View.VISIBLE);
@@ -77,17 +82,20 @@ public class LikingMyFragment extends BaseFragment implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        if (v == mInviteFriendsLayout){
+        if (v == mHeadInfoLayout) {
+            Intent intent = new Intent(getActivity(), MyInfoActivity.class);
+            startActivity(intent);
+        } else if (v == mInviteFriendsLayout) {
 
-        }else if (v == mCouponsLayout){
+        } else if (v == mCouponsLayout) {
 
-        }else if (v == mContactJoinLayout){
+        } else if (v == mContactJoinLayout) {
             Intent intent = new Intent(getActivity(), ContactJonInActivity.class);
             startActivity(intent);
-        }else if (v == mBecomeTeacherLayout){
+        } else if (v == mBecomeTeacherLayout) {
             Intent intent = new Intent(getActivity(), BecomeTeacherActivity.class);
             startActivity(intent);
-        }else if (v == mAboutUsLayout){
+        } else if (v == mAboutUsLayout) {
             Intent intent = new Intent(getActivity(), AboutActivity.class);
             startActivity(intent);
         }
