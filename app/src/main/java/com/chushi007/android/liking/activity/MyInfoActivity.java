f9294b4fa154d51964d7b0f26340860990e4c579
shaozucheng <shaozucheng@chushi007.com>
2016-05-31 11:24:06 +0800

    简述：添加拍照选择照片功能
    背景信息：
    
    修改内容：

--- app/src/main/java/com/chushi007/android/liking/activity/MyInfoActivity.java
+++ app/src/main/java/com/chushi007/android/liking/activity/MyInfoActivity.java
@@ -1,12 +1,22 @@
 package com.chushi007.android.liking.activity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.aaron.android.framework.base.actionbar.AppBarActivity;
 import com.aaron.android.framework.library.imageloader.HImageView;
+
 import com.chushi007.android.liking.R;
+import com.chushi007.android.liking.dialog.CameraCustomDialog;
+import com.chushi007.android.liking.utils.ImageEnviromentUtil;
+import com.chushi007.android.liking.widgets.camera.CameraPhotoHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 说明:
@@ -14,8 +24,11 @@ import com.chushi007.android.liking.R;
  * Time:16/5/27 下午3:11
  */
 public class MyInfoActivity extends AppBarActivity implements View.OnClickListener {
+
     private RelativeLayout mHeadImageLayout;
     private HImageView mHeadImage;
+
+    private CameraPhotoHelper mCameraPhotoHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,15 @@ public class MyInfoActivity extends AppBarActivity implements View.OnClickListen
         setContentView(R.layout.activity_my_info);
         setTitle("XXX");
         initData();
+        mCameraPhotoHelper = new CameraPhotoHelper(this);
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mCameraPhotoHelper.onConfigurationChanged(newConfig);
+    }
+
 
     private void initData() {
         initView();
@@ -42,7 +63,50 @@ public class MyInfoActivity extends AppBarActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
         if (v == mHeadImageLayout) {
-
+            showCameraDialog();
         }
+    }
+
+    /**
+     * 显示拍照和选择相册dialog
+     */
+    public void showCameraDialog() {
+        final CameraCustomDialog cameraDialog = new CameraCustomDialog(this);
+        cameraDialog.setTextViewOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.dialog_photograph://拍照
+                        mCameraPhotoHelper.takePhotoFromCamera();
+                        cameraDialog.dismiss();
+                        break;
+                    case R.id.dialog_album://从相册中选择
+                        mCameraPhotoHelper.selectSingleFormAlbum();
+                        cameraDialog.dismiss();
+                        break;
+                    case R.id.dialog_cancel_btn:
+                        cameraDialog.dismiss();
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCameraPhotoHelper.onActivityResult(requestCode, resultCode, data, new CameraPhotoHelper.CameraPhotoCallBack() {
+            @Override
+            public void takePictureFromCamera(String imagePath) {
+                Bitmap mBitmap = ImageEnviromentUtil.compressImageSize(imagePath);
+                mHeadImage.setImageBitmap(mBitmap);
+            }
+
+            @Override
+            public void takePictureFromGallery(ArrayList<String> imagePathList) {
+                List<Bitmap> bitmapList = ImageEnviromentUtil.getAlbumBitmapList(imagePathList);
+                mHeadImage.setImageBitmap(bitmapList.get(0));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/chushi007/android/liking/widgets/camera/AlbumAdapter.java
@@ -0,0 +1,91 @@
+package com.chushi007.android.liking.widgets.camera;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.aaron.android.framework.base.widget.listview.HBaseAdapter;
+import com.aaron.android.thirdparty.camera.ImageLoader;
+import com.aaron.android.thirdparty.camera.SelectImageFromAlbum;
+import com.chushi007.android.liking.R;
+
+import java.util.ArrayList;
+
+/**
+ * 说明:展示扫描后的图片适配器
+ * Author shaozucheng
+ * Time:16/5/30 下午5:03
+ */
+public class AlbumAdapter extends HBaseAdapter<String> implements SelectImageFromAlbum {
+    private Context mContext;
+    private String mImageDirPath;//图片文件夹路径
+    private int mNeedSelectAmount;//需要选择的图片数量
+    //用户选择的图片，存储为图片的完整路径
+    private ArrayList<String> mSelectedImage = new ArrayList<>();
+
+    public AlbumAdapter(Context context) {
+        super(context);
+        this.mContext = context;
+    }
+
+    public void setImageDirPath(String imageDirPath, int needSelectAmount) {
+        mImageDirPath = imageDirPath;
+        mNeedSelectAmount = needSelectAmount;
+    }
+
+    @Override
+    protected BaseViewHolder<String> createViewHolder() {
+        return new AlbumViewHolder();
+    }
+
+    @Override
+    public ArrayList<String> getSelectImageFormAlbum() {
+        return mSelectedImage;
+    }
+
+
+    class AlbumViewHolder extends BaseViewHolder<String> {
+        View mRootView;
+        ImageView mImageView;
+        ImageButton mImageButton;
+
+        @Override
+        public View inflateItemView() {
+            mRootView = LayoutInflater.from(mContext).inflate(R.layout.grid_item, null, false);
+            mImageView = (ImageView) mRootView.findViewById(R.id.id_item_image);
+            mImageButton = (ImageButton) mRootView.findViewById(R.id.id_item_select);
+            return mRootView;
+        }
+
+        @Override
+        public void bindViews(final String dirPath) {
+            ImageLoader.getInstance(3, ImageLoader.Type.LIFO).loadImage(mImageDirPath + "/" + dirPath, mImageView);
+            mImageButton.setImageResource(R.drawable.picture_unselected);
+            mImageView.setColorFilter(null);
+            mImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 已经选择过该图片
+                    if (mSelectedImage.contains(mImageDirPath + "/" + dirPath)) {
+                        mSelectedImage.remove(mImageDirPath + "/" + dirPath);
+                        mImageButton.setImageResource(R.drawable.picture_unselected);
+                        mImageView.setColorFilter(null);
+                    } else {// 未选择该图片
+                        if (mSelectedImage.size() >= mNeedSelectAmount) {
+                            Toast.makeText(mContext, "最多可选择" + mNeedSelectAmount + "张图片", Toast.LENGTH_LONG).show();
+                        } else {
+                            mSelectedImage.add(mImageDirPath + "/" + dirPath);
+                            mImageButton.setImageResource(R.drawable.pictures_selected);
+                            mImageView.setColorFilter(Color.parseColor("#77000000"));
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+}
