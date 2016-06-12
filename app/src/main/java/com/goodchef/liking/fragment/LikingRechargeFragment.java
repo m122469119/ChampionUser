7484052757d5df2b3672d19ea03cee7d018c2487
shaozucheng <shaozucheng@chushi007.com>
2016-06-12 11:31:19 +0800

    简述：添加base64处理图片工具类
    背景信息：
    
    修改内容：

--- app/src/main/java/com/goodchef/liking/fragment/LikingRechargeFragment.java
+++ app/src/main/java/com/goodchef/liking/fragment/LikingRechargeFragment.java
@@ -5,9 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.aaron.android.framework.base.BaseFragment;
+import com.goodchef.liking.R;
 
 /**
  * Created on 16/5/20.
@@ -16,11 +17,30 @@ import com.aaron.android.framework.base.BaseFragment;
  * @version 1.0.0
  */
 public class LikingRechargeFragment extends BaseFragment {
+
+    ImageView mImageView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("LikingRechargeFragment");
-        return textView;
+        View view = inflater.inflate(R.layout.fragment_buy_card, container, false);
+        initView(view);
+        return view;
     }
+
+    private void initView(View view) {
+        mImageView = (ImageView) view.findViewById(R.id.buy_card_image);
+      //  Bitmap bitmap = BitmapBase64Util.stringToBitmap("iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyhpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuNi1jMDY3IDc5LjE1Nzc0NywgMjAxNS8wMy8zMC0yMzo0MDo0MiAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENDIDIwMTUgKE1hY2ludG9zaCkiIHhtcE1NOkluc3RhbmNlSUQ9InhtcC5paWQ6MkNEMDBFNzMxNjdCMTFFNkI3NzJFRDJEQzc1NTYyQkQiIHhtcE1NOkRvY3VtZW50SUQ9InhtcC5kaWQ6MkNEMDBFNzQxNjdCMTFFNkI3NzJFRDJEQzc1NTYyQkQiPiA8eG1wTU06RGVyaXZlZEZyb20gc3RSZWY6aW5zdGFuY2VJRD0ieG1wLmlpZDoyQ0QwMEU3MTE2N0IxMUU2Qjc3MkVEMkRDNzU1NjJCRCIgc3RSZWY6ZG9jdW1lbnRJRD0ieG1wLmRpZDoyQ0QwMEU3MjE2N0IxMUU2Qjc3MkVEMkRDNzU1NjJCRCIvPiA8L3JkZjpEZXNjcmlwdGlvbj4gPC9yZGY6UkRGPiA8L3g6eG1wbWV0YT4gPD94cGFja2V0IGVuZD0iciI/PrAXsI8AAADCSURBVHjaYvz//z/DYAZMDIMcjDqQUsACIlTe1uFVdEe4aWAdCAVsQNwNxDFAzAjEi4G4FIh/wRQQ8gglAFcgIDuwC4jzkPgg9ncgrkDTQ4tsz0hMGozFIp86mklIcOBiLPJzBkUuhoIyaFqIRXJwLSnphRaAEVTVDeZihnG0Lh514GhdPIjrYmI8PpB1MSOpaXC0Lh6ti0fr4tG6eNSBo3XxEO4X4wqYwdIvxllXj9bFo3XxaF08WhePOnCQA4AAAwDKW1Cu6dwYpgAAAABJRU5ErkJggg==");
+       // mImageView.setImageBitmap(bitmap);
+       // String str = BitmapBase64Util.bitmapToString(bitmap);
+       // Log.e("base64", str);
+    }
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/goodchef/liking/utils/BitmapBase64Util.java
@@ -0,0 +1,99 @@
+package com.goodchef.liking.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * 说明:
+ * Author shaozucheng
+ * Time:16/6/12 上午11:16
+ */
+public class BitmapBase64Util {
+
+    /**
+     * 字符转转成Bitmap
+     *
+     * @param string base64字符串
+     * @return bitmap 图片文件
+     */
+    public static Bitmap stringToBitmap(String string) {
+        //将字符串转换成Bitmap类型
+        Bitmap bitmap = null;
+        try {
+            byte[] bitmapArray;
+            bitmapArray = Base64.decode(string, Base64.DEFAULT);
+            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+
+    /***
+     * Bitmap（图片） 转成base64字符串
+     *
+     * @param bitmap 图片文件
+     * @return string base64字符串
+     */
+    public static String bitmapToString(Bitmap bitmap) {
+        //将Bitmap转换成字符串
+        String string = null;
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bStream);
+        byte[] bytes = bStream.toByteArray();
+        string = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return string;
+    }
+
+    /**
+     * Bitmap（图片）转成base64字符串
+     *
+     * @param imgPath 图片路径
+     * @return
+     */
+    public static String bitmapToString(String imgPath) {
+        Bitmap bitmap =null;
+        if (imgPath != null && imgPath.length() > 0) {
+            bitmap = readBitmap(imgPath);
+        }
+        if (bitmap == null) {
+            return null;
+        }
+        ByteArrayOutputStream out = null;
+        try {
+            out = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            out.flush();
+            out.close();
+
+            byte[] imgBytes = out.toByteArray();
+            return Base64.encodeToString(imgBytes, Base64.DEFAULT);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            return null;
+        } finally {
+            try {
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Bitmap readBitmap(String imgPath) {
+        try {
+            return BitmapFactory.decodeFile(imgPath);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
