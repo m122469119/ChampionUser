5b40818f32c2a64b3e6c6cf82e3c240411faf74b
shaozucheng <shaozucheng@chushi007.com>
2016-06-28 17:43:50 +0800

    简述：添加我的订单，营养餐列表界面和获取列表接口
    背景信息：
    
    修改内容：

---
+++ app/src/main/java/com/goodchef/liking/fragment/MyCardOrderFragment.java
@@ -0,0 +1,20 @@
+package com.goodchef.liking.fragment;
+
+import com.aaron.android.framework.base.widget.refresh.NetworkPagerLoaderRecyclerViewFragment;
+
+/**
+ * 说明:
+ * Author shaozucheng
+ * Time:16/6/28 下午2:54
+ */
+public class MyCardOrderFragment extends NetworkPagerLoaderRecyclerViewFragment {
+    @Override
+    protected void requestData(int page) {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
