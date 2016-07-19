cf662531a4c2ef31587a2b57db8091e9851b89cc
shaozucheng <shaozucheng@chushi007.com>
2016-07-19 10:53:12 +0800

    简述：修改首页无网络情况
    背景信息：
    
    修改内容：

---
+++ app/src/main/java/com/goodchef/liking/eventmessages/LikingHomeNoNetWorkMessage.java
@@ -0,0 +1,11 @@
+package com.goodchef.liking.eventmessages;
+
+import com.aaron.android.framework.base.eventbus.BaseMessage;
+
+/**
+ * 说明:
+ * Author shaozucheng
+ * Time:16/7/19 上午10:44
+ */
+public class LikingHomeNoNetWorkMessage extends BaseMessage{
+}
