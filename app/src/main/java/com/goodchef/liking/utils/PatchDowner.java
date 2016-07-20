a839c04f4f65696aa411f505f60c06641a8565d1
shaozucheng <shaozucheng@chushi007.com>
2016-07-20 10:53:40 +0800

    简述：修改升级卡bug
    背景信息：
    
    修改内容：

--- app/src/main/java/com/goodchef/liking/utils/PatchDowner.java
+++ app/src/main/java/com/goodchef/liking/utils/PatchDowner.java
@@ -28,7 +28,7 @@ public class PatchDowner extends AsyncTask<PatchData, Integer, PatchData> {
 
     public PatchDowner(Context context) {
         this.context = context;
-        patchDirPath = DiskStorageManager.getInstance().getFilePath() + "patch/" ;
+        patchDirPath = DiskStorageManager.getInstance().getFilePath() + "patch/";
         File patchDir = FileUtils.createFolder(patchDirPath);
         if (patchDir == null) {
             LogUtils.e(TAG, "patchDir create fail");
@@ -39,38 +39,40 @@ public class PatchDowner extends AsyncTask<PatchData, Integer, PatchData> {
 
     @Override
     protected PatchData doInBackground(PatchData... params) {
-        String urlS = params[0].getPatchUrl();
-        if (!TextUtils.isEmpty(urlS)) {
-            patchName = urlS.substring(urlS.lastIndexOf("/"));
-            File patchFile = new File(patchDirPath, patchName);
-            if (patchFile.exists()) {
-                LogUtils.i(TAG, "补丁存在");
-                params[0].setPatchFile(patchFile.getAbsolutePath());
-                LogUtils.d(TAG, "patchFile: " + patchFile.getAbsolutePath());
-                return params[0];
-            } else {
-                LogUtils.i("Dust", "补丁不存在，开始下载");
-                try {
-                    URL url = new URL(urlS);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    int responseCode = conn.getResponseCode();
-                    if (responseCode == 200) {
-                        FileOutputStream fos = new FileOutputStream(patchFile);
-                        InputStream inStream = conn.getInputStream();
-                        byte[] buff = new byte[1024];
-                        int len = 0;
-                        while ((len = inStream.read(buff)) != -1) {
-                            fos.write(buff, 0, len);
+        if (params[0] != null) {
+            String urlS = params[0].getPatchUrl();
+            if (!TextUtils.isEmpty(urlS)) {
+                patchName = urlS.substring(urlS.lastIndexOf("/"));
+                File patchFile = new File(patchDirPath, patchName);
+                if (patchFile.exists()) {
+                    LogUtils.i(TAG, "补丁存在");
+                    params[0].setPatchFile(patchFile.getAbsolutePath());
+                    LogUtils.d(TAG, "patchFile: " + patchFile.getAbsolutePath());
+                    return params[0];
+                } else {
+                    LogUtils.i("Dust", "补丁不存在，开始下载");
+                    try {
+                        URL url = new URL(urlS);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setRequestMethod("GET");
+                        int responseCode = conn.getResponseCode();
+                        if (responseCode == 200) {
+                            FileOutputStream fos = new FileOutputStream(patchFile);
+                            InputStream inStream = conn.getInputStream();
+                            byte[] buff = new byte[1024];
+                            int len = 0;
+                            while ((len = inStream.read(buff)) != -1) {
+                                fos.write(buff, 0, len);
+                            }
+                            inStream.close();
+                            fos.close();
+                            params[0].setPatchFile(patchFile.getAbsolutePath());
+                            LogUtils.d(TAG, "patchFile: " + patchFile.getAbsolutePath());
+                            return params[0];
                         }
-                        inStream.close();
-                        fos.close();
-                        params[0].setPatchFile(patchFile.getAbsolutePath());
-                        LogUtils.d(TAG, "patchFile: " + patchFile.getAbsolutePath());
-                        return params[0];
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             }
         }
