8d07c92f0adfd9e9a515f0d2c9142162293fc8da
shaozucheng <shaozucheng@likingfit.com>
2017-01-04 14:29:37 +0800

    简述：修改联系加盟和成为教练中的选择城市
    背景信息：
    
    修改内容：

--- app/src/main/java/com/goodchef/liking/activity/ContactJonInActivity.java
+++ app/src/main/java/com/goodchef/liking/activity/ContactJonInActivity.java
@@ -1,6 +1,9 @@
 package com.goodchef.liking.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +13,8 @@ import com.aaron.android.framework.base.ui.actionbar.AppBarActivity;
 import com.aaron.android.codelibrary.utils.RegularUtils;
 import com.aaron.android.framework.utils.PopupUtils;
 import com.goodchef.liking.R;
+import com.goodchef.liking.dialog.SelectCityDialog;
+import com.goodchef.liking.http.result.data.City;
 import com.goodchef.liking.mvp.presenter.ContactJoinPresenter;
 import com.goodchef.liking.mvp.view.ContactJoinView;
 
@@ -21,7 +26,7 @@ import com.goodchef.liking.mvp.view.ContactJoinView;
 public class ContactJonInActivity extends AppBarActivity implements View.OnClickListener, ContactJoinView {
     private EditText mNameEditText;
     private EditText mPhoneEditText;
-    private EditText mCityEditText;
+    private TextView mCityTextView;
     private TextView mImmediatelySubmitBtn;
 
     private ContactJoinPresenter mContactJoinPresenter;
@@ -37,47 +42,109 @@ public class ContactJonInActivity extends AppBarActivity implements View.OnClick
     private void initView() {
         mNameEditText = (EditText) findViewById(R.id.name_editText);
         mPhoneEditText = (EditText) findViewById(R.id.phone_editText);
-        mCityEditText = (EditText) findViewById(R.id.city_editText);
+        mCityTextView = (TextView) findViewById(R.id.city_editText);
         mImmediatelySubmitBtn = (TextView) findViewById(R.id.immediately_submit);
 
         mImmediatelySubmitBtn.setOnClickListener(this);
+        mCityTextView.setOnClickListener(this);
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (mNameEditText.requestFocus()) {
+                    mNameEditText.setBackgroundResource(R.drawable.shape_four_card_green_background);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPhoneEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (mPhoneEditText.requestFocus()) {
+                    mPhoneEditText.setBackgroundResource(R.drawable.shape_four_card_green_background);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
     @Override
     public void onClick(View v) {
         if (v == mImmediatelySubmitBtn) {
             sendConfirmRequest();
+        } else if (v == mCityTextView) {
+            showSelectCityDialog();
         }
+    }
+
+    private void showSelectCityDialog() {
+        SelectCityDialog dialog = new SelectCityDialog(this);
+        dialog.setNegativeClickListener(new SelectCityDialog.cancelClickListener() {
+            @Override
+            public void onCancelClickListener(AppCompatDialog dialog) {
+                dialog.dismiss();
+            }
+        });
+        dialog.setPositiveClickListener(new SelectCityDialog.confirmClickListener() {
+            @Override
+            public void OnConfirmClickListener(AppCompatDialog dialog, City.RegionsData regionsData, City.RegionsData.CitiesData citiesData) {
+                mCityTextView.setText(regionsData.getProvinceName() + " - " + citiesData.getCityName());
+                dialog.dismiss();
+            }
+        });
     }
 
     private void sendConfirmRequest() {
         String name = mNameEditText.getText().toString().trim();
         String phone = mPhoneEditText.getText().toString().trim();
-        String city = mCityEditText.getText().toString().trim();
+        String city = mCityTextView.getText().toString();
 
         if (StringUtils.isEmpty(name)) {
             PopupUtils.showToast("姓名不能为空");
+            mNameEditText.setBackgroundResource(R.drawable.shape_four_card_red_background);
             return;
         } else if (name.length() > 15) {
             PopupUtils.showToast("姓名不能超过15个字");
+            mNameEditText.setBackgroundResource(R.drawable.shape_four_card_red_background);
             return;
         }
         if (StringUtils.isEmpty(phone)) {
             PopupUtils.showToast("手机号不能为空");
+            mPhoneEditText.setBackgroundResource(R.drawable.shape_four_card_red_background);
             return;
         }
         if (!RegularUtils.isMobileExact(phone)) {
             PopupUtils.showToast("手机号码输入不正确");
+            mPhoneEditText.setBackgroundResource(R.drawable.shape_four_card_red_background);
             return;
         }
         if (StringUtils.isEmpty(city)) {
-            PopupUtils.showToast("城市不能为空");
-            return;
-        } else if (city.length() > 20) {
-            PopupUtils.showToast("城市不能超过20个字");
+            PopupUtils.showToast("请选择城市");
             return;
         }
-        mContactJoinPresenter = new ContactJoinPresenter(this, this);
+
+        if (mContactJoinPresenter == null) {
+            mContactJoinPresenter = new ContactJoinPresenter(this, this);
+        }
         mContactJoinPresenter.joinAllpy(name, phone, city, 0);
     }
 
@@ -86,7 +153,7 @@ public class ContactJonInActivity extends AppBarActivity implements View.OnClick
         PopupUtils.showToast("提交成功,请等待商务人员与您联系");
         mNameEditText.setText("");
         mPhoneEditText.setText("");
-        mCityEditText.setText("");
+        mCityTextView.setText("");
         finish();
     }
 }
---
+++ app/src/main/java/com/goodchef/liking/dialog/SelectCityDialog.java
@@ -0,0 +1,192 @@
+package com.goodchef.liking.dialog;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.aaron.android.framework.utils.DialogUtils;
+import com.goodchef.liking.R;
+import com.goodchef.liking.http.result.data.City;
+import com.goodchef.liking.utils.CityUtils;
+import com.goodchef.liking.widgets.wheel.OnWheelChangedListener;
+import com.goodchef.liking.widgets.wheel.WheelView;
+import com.goodchef.liking.widgets.wheel.adapters.AbstractWheelTextAdapter;
+
+import java.util.List;
+
+/**
+ * 说明:
+ * Author : shaozucheng
+ * Time: 下午5:05
+ * version 1.0.0
+ */
+
+public class SelectCityDialog {
+
+    private Context mContext;
+    private AppCompatDialog mDialog;
+    private TextView mCancelButton;
+    private TextView mConfirmButton;
+    private WheelView mProvinceWheelView;
+    private WheelView mCityWheelView;
+
+    private List<City.RegionsData> RegionsList;
+
+    public SelectCityDialog(Context context) {
+        this.mContext = context;
+        mDialog = new AppCompatDialog(context, R.style.camera_dialog_no_screen);
+        mDialog.setCancelable(true);
+        mDialog.setCanceledOnTouchOutside(true);
+        mDialog.show();
+        Window window = mDialog.getWindow();
+        window.setContentView(R.layout.dialog_city_view);
+        window.setWindowAnimations(R.style.my_dialog_enter_exit);  //添加dialog进入和退出的动画
+
+        DialogUtils.resetDialogScreenPosition(mDialog, Gravity.BOTTOM, 0, 0, WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+
+        mCancelButton = (TextView) window.findViewById(R.id.dialog_cancel_button);
+        mConfirmButton = (TextView) window.findViewById(R.id.dialog_confirm_button);
+        mProvinceWheelView = (WheelView) window.findViewById(R.id.select_province_WheelView);
+        mCityWheelView = (WheelView) window.findViewById(R.id.select_city_WheelView);
+
+        initData();
+    }
+
+    private void initData() {
+        RegionsList = CityUtils.getLocalCityList(mContext);
+        mProvinceWheelView.setVisibleItems(3);
+        mProvinceWheelView.setCurrentItem(RegionsList.size() / 2);
+        mProvinceWheelView.setCyclic(false);
+        mProvinceWheelView.setWheelLine(R.color.split_line_color);
+        mCityWheelView.setCyclic(false);
+        mCityWheelView.setVisibleItems(3);
+        mCityWheelView.setWheelLine(R.color.split_line_color);
+
+        mProvinceWheelView.setViewAdapter(new RegionAdapter(mContext, RegionsList));
+
+        mCityWheelView.setViewAdapter(new CitiesAdapter(mContext, RegionsList.get(0).getCities()));
+        mCityWheelView.setCurrentItem(RegionsList.get(0).getCities().size() / 2);
+
+        mProvinceWheelView.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                List<City.RegionsData.CitiesData> citiesList = RegionsList.get(newValue).getCities();
+                mCityWheelView.setViewAdapter(new CitiesAdapter(mContext, citiesList));
+                mCityWheelView.setCurrentItem(citiesList.size() / 2);
+            }
+        });
+    }
+
+    public void setNegativeClickListener(final cancelClickListener onClickListener) {
+        if (mCancelButton != null) {
+            mCancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickListener.onCancelClickListener(mDialog);
+                }
+            });
+        }
+    }
+
+    public void setPositiveClickListener(final confirmClickListener confirmClickListener) {
+        if (mConfirmButton != null) {
+            mConfirmButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    City.RegionsData regionsData = RegionsList.get(mProvinceWheelView.getCurrentItem());
+                    City.RegionsData.CitiesData citiesData = regionsData.getCities().get(mCityWheelView.getCurrentItem());
+                    confirmClickListener.OnConfirmClickListener(mDialog, regionsData, citiesData);
+                }
+            });
+        }
+    }
+
+    public interface confirmClickListener {
+        void OnConfirmClickListener(AppCompatDialog dialog, City.RegionsData regionsData, City.RegionsData.CitiesData citiesData);
+    }
+
+    public interface cancelClickListener {
+        void onCancelClickListener(AppCompatDialog dialog);
+    }
+
+
+    /**
+     * 设置是否可以关闭
+     *
+     * @param cancelable true或false
+     */
+    public void setCancelable(boolean cancelable) {
+        mDialog.setCancelable(cancelable);
+    }
+
+    /**
+     * 设置是否可以点击dialog外面关闭dialog
+     *
+     * @param canceledOnTouchOutside true或false
+     */
+    public void setCanceledOnTouchOutside(boolean canceledOnTouchOutside) {
+        mDialog.setCanceledOnTouchOutside(canceledOnTouchOutside);
+    }
+
+    /**
+     * 关闭对话框
+     */
+    public void dismiss() {
+        mDialog.dismiss();
+    }
+
+
+    public class RegionAdapter extends AbstractWheelTextAdapter {
+
+        private List<City.RegionsData> regionsList;
+
+        public RegionAdapter(Context context, List<City.RegionsData> mList) {
+            super(context);
+            this.regionsList = mList;
+        }
+
+        @Override
+        protected CharSequence getItemText(int index) {
+            if (index >= 0 && index < regionsList.size()) {
+                City.RegionsData bank = regionsList.get(index);
+                return bank.getProvinceName();
+            }
+            return null;
+        }
+
+        @Override
+        public int getItemsCount() {
+            return regionsList.size();
+        }
+    }
+
+    public class CitiesAdapter extends AbstractWheelTextAdapter {
+
+        private List<City.RegionsData.CitiesData> citiesList;
+
+        public CitiesAdapter(Context context, List<City.RegionsData.CitiesData> mList) {
+            super(context);
+            this.citiesList = mList;
+        }
+
+        @Override
+        protected CharSequence getItemText(int index) {
+            if (index >= 0 && index < citiesList.size()) {
+                City.RegionsData.CitiesData bankCard = citiesList.get(index);
+                return bankCard.getCityName();
+            }
+            return null;
+        }
+
+        @Override
+        public int getItemsCount() {
+            return citiesList.size();
+        }
+    }
+
+}
